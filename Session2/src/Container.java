7320505f651e8bc5435e47a13886b25fdc1113aa
Gurpreet <Gurpreet@Gurpreet-PC>
2019-05-18 19:51:18 +0530

    Container and Statement

---
+++ Session2/src/Container.java
@@ -0,0 +1,35 @@
+
+public class Container {
+
+	public static void main(String[] args) {
+       
+		byte age = 100;
+		
+		age = 12;
+		System.out.println("age is: "+age);
+		
+		age = (byte)256;
+		System.out.println("age now is: "+age);
+		
+		int num = 10000;
+		
+		long salary = 100000231210L;
+		
+		byte johnsAge = 10;
+		
+		System.out.println("num is: "+num);
+
+		
+		float pi = 3.14f;
+		double pi1 = 3.14;
+		
+		System.out.println("Salary is: "+salary);
+		
+		boolean flag = true; // 1
+		
+		char ch = 'G';
+		
+
+	}
+
+}
---
+++ Session2/src/Statement.java
@@ -0,0 +1,16 @@
+
+public class Statement {
+
+	public static void main(String[] args) {
+		         // Job1
+				System.out.println("This is Statement 1");
+
+				// Job2
+				System.out.println("This is Statement 2");
+				
+				// Job3
+				System.out.println("This is Statement 3");
+				
+		  }
+
+}
